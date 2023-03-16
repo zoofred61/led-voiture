a92b0df91f065ea14a0ba5954e1177b933180aca
zoofred61 <128042375+zoofred61@users.noreply.github.com>
2023-03-16 16:30:37 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,38 @@
+function clignote_droite () {
+    droite.showColor(neopixel.colors(NeoPixelColors.Orange))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    basic.pause(100)
+    droite.showColor(neopixel.colors(NeoPixelColors.Black))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    basic.pause(100)
+}
+function clignote_gauche () {
+    gauche.showColor(neopixel.colors(NeoPixelColors.Orange))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    basic.pause(100)
+    gauche.showColor(neopixel.colors(NeoPixelColors.Black))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    basic.pause(100)
+}
 input.onButtonPressed(Button.A, function () {
-    gauche.showColor(neopixel.colors(NeoPixelColors.Orange))
-    basic.pause(500)
-    gauche.showColor(neopixel.colors(NeoPixelColors.Black))
-    basic.pause(500)
-    gauche.showColor(neopixel.colors(NeoPixelColors.Orange))
-    basic.pause(500)
-    gauche.showColor(neopixel.colors(NeoPixelColors.Black))
-    basic.pause(500)
-    strip.clear()
+    for (let index = 0; index < 4; index++) {
+        clignote_gauche()
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     stop.showColor(neopixel.colors(NeoPixelColors.Red))
-    basic.pause(2000)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 3000, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
     stop.showColor(neopixel.colors(NeoPixelColors.Black))
 })
 input.onButtonPressed(Button.B, function () {
-    droite.showColor(neopixel.colors(NeoPixelColors.Orange))
-    basic.pause(500)
-    droite.showColor(neopixel.colors(NeoPixelColors.Black))
-    basic.pause(500)
-    droite.showColor(neopixel.colors(NeoPixelColors.Orange))
-    basic.pause(500)
-    droite.showColor(neopixel.colors(NeoPixelColors.Black))
-    basic.pause(500)
-    strip.clear()
+    for (let index = 0; index < 4; index++) {
+        clignote_droite()
+    }
 })
 let stop: neopixel.Strip = null
 let droite: neopixel.Strip = null
 let gauche: neopixel.Strip = null
-let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+let strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
 gauche = strip.range(0, 2)
 droite = strip.range(3, 2)
 stop = strip.range(0, 5)
